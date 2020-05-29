cc746da251a5adfdac1bf542ddcbf33cb6b30191
Sebastien Hoarau <gaarv1911@gmail.com>
2020-05-29 16:46:46 +0200

    fix start

--- start.sh
+++ start.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email=\"gaarv@users.noreply.github.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless=true\n\
-enableCORS=false\n\
-port=$PORT\n\
-" > ~/.streamlit/config.toml
-
-streamlit run app.py
+streamlit run app.py --server.port=$PORT --server.headless=true --server.enableCORS=false
